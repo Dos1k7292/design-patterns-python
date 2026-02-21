52cf17259d79a3029a19b18fe5686f8bcdbcb8d3
Dos1k7292 <konilkoshdosymzhan@gmail.com>
2026-02-21 23:29:00 +0500

    Create director.py

---
+++ builder/director.py
@@ -0,0 +1,6 @@
+class ReportDirector:
+
+    def construct(self, builder):
+        builder.set_header("Report Header")
+        builder.set_content("Report Content")
+        builder.set_footer("Report Footer")
